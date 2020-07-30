31595500ab07858130e587b85f258c4e5653df42
Jabir <jabir@cankado.com>
2020-07-30 18:46:15 +0530

    styles added for product

--- shopping_cart/urls.py
+++ shopping_cart/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.views.static import serve
+from django.conf import settings
 from django.conf.urls import include,url
 from accounts import views as accounts
 from shopping import views as shopping
@@ -11,4 +13,7 @@ urlpatterns = [
     url(r'^logout/$', accounts.user_logout, name='logout'),
     path('', shopping.homepage, name='homepage'),
     path('shopping/', include(("shopping.urls","shopping"))),
+
+    re_path('media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path('static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_FILE_ROOT}),
 ]
